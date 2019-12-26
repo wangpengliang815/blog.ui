3ce7e2c1149feb62046438d3fd1587820666150d
PengLiang Wang <wangpengliang815@163.com>
2019-12-26 19:18:32 +0800

    add components

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
+// 引入path模块
 const path = require("path");
+// 引入express模块
 const express = require("express");
+
 const app = express();
 
 app.locals.getFirstMenu = function(data) {
-  return data.filter(function(item) {
-    return item.parentId === 0;
-  });
+	return data.filter(function(item) {
+		return item.parentId === 0;
+	});
 };
 
 app.locals.getSecondMenu = function(data, id) {
-  return data.filter(function(item) {
-    return item.parentId === id;
-  });
+	return data.filter(function(item) {
+		return item.parentId === id;
+	});
 };
 
-app.set("views", path.join(__dirname, "views")); // 设置存放模板文件的目录
-app.set("view engine", "ejs"); // 设置模板引擎为 ejs
+// 设置存放模板文件的目录
+app.set("views", path.join(__dirname, "views"));
+// 设置模板引擎为 ejs
+app.set("view engine", "ejs");
 
+// 挂载根路由控制器
 const indexRouter = require("./routes/index");
 const userRouter = require("./routes/user");
 
